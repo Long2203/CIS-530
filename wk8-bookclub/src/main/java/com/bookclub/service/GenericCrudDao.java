37e2e60910b78c593feb91a9926490e57d556bec
Long2203 <longuyen@my365.bellevue.edu>
2023-10-21 13:55:38 -0400

    Week8-Commit.

---
+++ wk8-bookclub/src/main/java/com/bookclub/service/GenericCrudDao.java
@@ -0,0 +1,15 @@
+/*
+   Krasso, R., (2021). CIS 505 Intermediate Java Programming. Bellevue University, all rights reserved.
+Additional modifications by Nguyen, L. Student 2023
+*/
+package com.bookclub.service;
+
+import java.util.List;
+
+public interface GenericCrudDao<E, K> {
+    void add(E entity);
+    void update(E entity);
+    boolean remove(E entity);
+    List<E> list(); // Return a list of objects of type E.
+    E find(K key); // Return an object of type E by type K value.
+}
