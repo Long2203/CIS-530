840481c7bd8d0ec1486dc8954429df1ee21fffb5
Long2203 <longuyen@my365.bellevue.edu>
2023-10-26 17:33:41 -0400

    Week9-commit.

---
+++ wk9-bookclub/src/main/java/com/bookclub/BookclubApplication.java
@@ -0,0 +1,17 @@
+/*
+   Krasso, R., (2021). CIS 505 Intermediate Java Programming. Bellevue University, all rights reserved.
+Additional modifications by Nguyen, L. Student 2023
+*/
+package com.bookclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookclubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookclubApplication.class, args);
+	}
+
+}
